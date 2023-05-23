bfe1b0307f8e9e32aad80ecfe87b30f7d0273446
BryanHGRoc <bryan.rocha2@etec.gov.sp.br>
2023-05-23 12:19:43 -0300

    começamos o pac man

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
+//circulo amarelo (pac-man)
+ctx.strokeStyle ='yellow';
 ctx.beginPath();
-    ctx.arc(350,300,50,0, Math.PI * 2, true);
-    ctx.moveTo(110,75);
-    ctx.fillStyle()
+    ctx.arc(350,300,250,0, Math.PI * 2, true);
+    ctx.moveTo(810,275);
+    ctx.fillStyle = 'yellow';
+    ctx.fill();
     ctx.stroke();
+
+//triangulo (boca do pac-man)
+ctx.strokeStyle ='#020612';
+ctx.beginPath();
+    ctx.moveTo(75,50);
+    ctx.lineTo(200,150);
+    ctx.lineTo(200,50);
+    ctx.fillStyle = '#020612';
+    ctx.fill();
