44aaead9724c53842f36707514806a933f11a475
BryaHGRoc <127855127+BryanHGRoc@users.noreply.github.com>
2023-06-04 22:30:48 -0300

    Terminado o nosso Pac-Man

--- script.js
+++ script.js
@@ -6,19 +6,39 @@ const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
 //circulo amarelo (pac-man)
-ctx.strokeStyle ='yellow';
 ctx.beginPath();
-    ctx.arc(350,300,250,0, Math.PI * 2, true);
-    ctx.moveTo(810,275);
-    ctx.fillStyle = 'yellow';
+    ctx.arc(200, 200, 150, 0.2 * Math.PI, 1.8 * Math.PI);
+    ctx.lineTo(210, 200);
+    ctx.fillStyle = "yellow";
     ctx.fill();
-    ctx.stroke();
 
-//triangulo (boca do pac-man)
-ctx.strokeStyle ='#020612';
+//Bolinhas
 ctx.beginPath();
-    ctx.moveTo(75,50);
-    ctx.lineTo(200,150);
-    ctx.lineTo(200,50);
-    ctx.fillStyle = '#020612';
+    ctx.arc(500, 200, 55, 0, Math.PI * 2, true);
+    ctx.moveTo(1000,1000);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+
+ctx.beginPath();
+    ctx.arc(800, 200, 55, 0, Math.PI * 2, true);
+    ctx.moveTo(1000,1000);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+
+ctx.beginPath();
+    ctx.arc(1100, 200, 55, 0, Math.PI * 2, true);
+    ctx.moveTo(1000,1000);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+
+ctx.beginPath();
+    ctx.arc(1400, 200, 55, 0, Math.PI * 2, true);
+    ctx.moveTo(1000,1000);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+
+ctx.beginPath();
+    ctx.arc(1700, 200, 55, 0, Math.PI * 2, true);
+    ctx.moveTo(1000,1000);
+    ctx.fillStyle = "yellow";
     ctx.fill();
